c807fc8fec5908e6310fdd6f44b85dd5278e2509
userfiona <fionaramaesela@gmail.com>
2023-11-04 00:14:26 -0700

    download

--- web_dynamic/4-hbnb.py
+++ web_dynamic/4-hbnb.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Starts a Flask Web Application """
+""" Starts a Flash Web Application """
 from models import storage
 from models.state import State
 from models.city import City
@@ -9,14 +9,17 @@ from os import environ
 from flask import Flask, render_template
 import uuid
 
-app = Flask(__name)
+
+app = Flask(_name_)
+
 
 @app.teardown_appcontext
 def close_db(error):
     """ Remove the current SQLAlchemy Session """
     storage.close()
 
-@app.route('/3-hbnb', strict_slashes=False)  # Change the route to '/3-hbnb'
+
+@app.route('/4-hbnb/', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -32,12 +35,13 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    return render_template('2-hbnb.html',
+    return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,
                            cache_id=uuid.uuid4())
 
-if __name__ == "__main":
+
+if _name_ == "_main_":
     """ Main Function """
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5001)
